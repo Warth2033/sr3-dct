27f36ed4be8457f1498b274edc0798362b9e21fe
warth <warthcx@gmail.com>
2023-09-09 20:05:39 +0800

    注释unet.py

--- model/sr3_modules/unet.py
+++ model/sr3_modules/unet.py
@@ -5,16 +5,19 @@ import torch.nn.functional as F
 from inspect import isfunction
 
 
+# 检查对象是否存在
 def exists(x):
     return x is not None
 
 
+# 如果值存在，则返回该值，否则返回默认值
 def default(val, d):
     if exists(val):
         return val
     return d() if isfunction(d) else d
 
-# PositionalEncoding Source： https://github.com/lmnt-com/wavegrad/blob/master/src/wavegrad/model.py
+
+# 位置编码类 Source： https://github.com/lmnt-com/wavegrad/blob/master/src/wavegrad/model.py
 class PositionalEncoding(nn.Module):
     def __init__(self, dim):
         super().__init__()
@@ -22,39 +25,45 @@ class PositionalEncoding(nn.Module):
 
     def forward(self, noise_level):
         count = self.dim // 2
-        step = torch.arange(count, dtype=noise_level.dtype,
-                            device=noise_level.device) / count
-        encoding = noise_level.unsqueeze(
-            1) * torch.exp(-math.log(1e4) * step.unsqueeze(0))
-        encoding = torch.cat(
-            [torch.sin(encoding), torch.cos(encoding)], dim=-1)
+        step = (
+            torch.arange(count, dtype=noise_level.dtype, device=noise_level.device)
+            / count
+        )
+        encoding = noise_level.unsqueeze(1) * torch.exp(
+            -math.log(1e4) * step.unsqueeze(0)
+        )
+        encoding = torch.cat([torch.sin(encoding), torch.cos(encoding)], dim=-1)
         return encoding
 
 
+# 特征级别的仿射变换
 class FeatureWiseAffine(nn.Module):
     def __init__(self, in_channels, out_channels, use_affine_level=False):
         super(FeatureWiseAffine, self).__init__()
         self.use_affine_level = use_affine_level
         self.noise_func = nn.Sequential(
-            nn.Linear(in_channels, out_channels*(1+self.use_affine_level))
+            nn.Linear(in_channels, out_channels * (1 + self.use_affine_level))
         )
 
     def forward(self, x, noise_embed):
         batch = x.shape[0]
         if self.use_affine_level:
-            gamma, beta = self.noise_func(noise_embed).view(
-                batch, -1, 1, 1).chunk(2, dim=1)
+            gamma, beta = (
+                self.noise_func(noise_embed).view(batch, -1, 1, 1).chunk(2, dim=1)
+            )
             x = (1 + gamma) * x + beta
         else:
             x = x + self.noise_func(noise_embed).view(batch, -1, 1, 1)
         return x
 
 
+# Swish激活函数
 class Swish(nn.Module):
     def forward(self, x):
         return x * torch.sigmoid(x)
 
 
+# 上采样模块
 class Upsample(nn.Module):
     def __init__(self, dim):
         super().__init__()
@@ -65,6 +74,7 @@ class Upsample(nn.Module):
         return self.conv(self.up(x))
 
 
+# 下采样模块
 class Downsample(nn.Module):
     def __init__(self, dim):
         super().__init__()
@@ -77,6 +87,7 @@ class Downsample(nn.Module):
 # building block modules
 
 
+# 基础块模块
 class Block(nn.Module):
     def __init__(self, dim, dim_out, groups=32, dropout=0):
         super().__init__()
@@ -84,23 +95,32 @@ class Block(nn.Module):
             nn.GroupNorm(groups, dim),
             Swish(),
             nn.Dropout(dropout) if dropout != 0 else nn.Identity(),
-            nn.Conv2d(dim, dim_out, 3, padding=1)
+            nn.Conv2d(dim, dim_out, 3, padding=1),
         )
 
     def forward(self, x):
         return self.block(x)
 
 
+# Resnet块模块
 class ResnetBlock(nn.Module):
-    def __init__(self, dim, dim_out, noise_level_emb_dim=None, dropout=0, use_affine_level=False, norm_groups=32):
+    def __init__(
+        self,
+        dim,
+        dim_out,
+        noise_level_emb_dim=None,
+        dropout=0,
+        use_affine_level=False,
+        norm_groups=32,
+    ):
         super().__init__()
         self.noise_func = FeatureWiseAffine(
-            noise_level_emb_dim, dim_out, use_affine_level)
+            noise_level_emb_dim, dim_out, use_affine_level
+        )
 
         self.block1 = Block(dim, dim_out, groups=norm_groups)
         self.block2 = Block(dim_out, dim_out, groups=norm_groups, dropout=dropout)
-        self.res_conv = nn.Conv2d(
-            dim, dim_out, 1) if dim != dim_out else nn.Identity()
+        self.res_conv = nn.Conv2d(dim, dim_out, 1) if dim != dim_out else nn.Identity()
 
     def forward(self, x, time_emb):
         b, c, h, w = x.shape
@@ -110,6 +130,7 @@ class ResnetBlock(nn.Module):
         return h + self.res_conv(x)
 
 
+# 自注意力模块
 class SelfAttention(nn.Module):
     def __init__(self, in_channel, n_head=1, norm_groups=32):
         super().__init__()
@@ -142,101 +163,151 @@ class SelfAttention(nn.Module):
         return out + input
 
 
+# 带有注意力的Resnet块
 class ResnetBlocWithAttn(nn.Module):
-    def __init__(self, dim, dim_out, *, noise_level_emb_dim=None, norm_groups=32, dropout=0, with_attn=False):
+    def __init__(
+        self,
+        dim,
+        dim_out,
+        *,
+        noise_level_emb_dim=None,
+        norm_groups=32,
+        dropout=0,
+        with_attn=False
+    ):
         super().__init__()
         self.with_attn = with_attn
         self.res_block = ResnetBlock(
-            dim, dim_out, noise_level_emb_dim, norm_groups=norm_groups, dropout=dropout)
+            dim, dim_out, noise_level_emb_dim, norm_groups=norm_groups, dropout=dropout
+        )
         if with_attn:
             self.attn = SelfAttention(dim_out, norm_groups=norm_groups)
 
     def forward(self, x, time_emb):
         x = self.res_block(x, time_emb)
-        if(self.with_attn):
+        if self.with_attn:
             x = self.attn(x)
         return x
 
 
+# UNet网络结构
 class UNet(nn.Module):
     def __init__(
         self,
-        in_channel=6,
-        out_channel=3,
-        inner_channel=32,
-        norm_groups=32,
-        channel_mults=(1, 2, 4, 8, 8),
-        attn_res=(8),
-        res_blocks=3,
-        dropout=0,
-        with_noise_level_emb=True,
-        image_size=128
+        in_channel=6,  # 输入通道数
+        out_channel=3,  # 输出通道数
+        inner_channel=32,  # 内部通道数
+        norm_groups=32,  # 归一化组数
+        channel_mults=(1, 2, 4, 8, 8),  # 通道倍数
+        attn_res=(8),  # 使用注意力机制的分辨率
+        res_blocks=3,  # 残差块数量
+        dropout=0,  # dropout率
+        with_noise_level_emb=True,  # 是否使用噪声级别嵌入
+        image_size=128,  # 图像大小
     ):
         super().__init__()
 
+        # 如果使用噪声级别嵌入
         if with_noise_level_emb:
             noise_level_channel = inner_channel
+            # 定义噪声级别的多层感知机
             self.noise_level_mlp = nn.Sequential(
                 PositionalEncoding(inner_channel),
                 nn.Linear(inner_channel, inner_channel * 4),
                 Swish(),
-                nn.Linear(inner_channel * 4, inner_channel)
+                nn.Linear(inner_channel * 4, inner_channel),
             )
         else:
             noise_level_channel = None
             self.noise_level_mlp = None
 
+        # 定义下采样层
         num_mults = len(channel_mults)
         pre_channel = inner_channel
         feat_channels = [pre_channel]
         now_res = image_size
-        downs = [nn.Conv2d(in_channel, inner_channel,
-                           kernel_size=3, padding=1)]
+        downs = [nn.Conv2d(in_channel, inner_channel, kernel_size=3, padding=1)]
         for ind in range(num_mults):
-            is_last = (ind == num_mults - 1)
-            use_attn = (now_res in attn_res)
+            is_last = ind == num_mults - 1
+            use_attn = now_res in attn_res
             channel_mult = inner_channel * channel_mults[ind]
             for _ in range(0, res_blocks):
-                downs.append(ResnetBlocWithAttn(
-                    pre_channel, channel_mult, noise_level_emb_dim=noise_level_channel, norm_groups=norm_groups, dropout=dropout, with_attn=use_attn))
+                downs.append(
+                    ResnetBlocWithAttn(
+                        pre_channel,
+                        channel_mult,
+                        noise_level_emb_dim=noise_level_channel,
+                        norm_groups=norm_groups,
+                        dropout=dropout,
+                        with_attn=use_attn,
+                    )
+                )
                 feat_channels.append(channel_mult)
                 pre_channel = channel_mult
             if not is_last:
                 downs.append(Downsample(pre_channel))
                 feat_channels.append(pre_channel)
-                now_res = now_res//2
+                now_res = now_res // 2
         self.downs = nn.ModuleList(downs)
 
-        self.mid = nn.ModuleList([
-            ResnetBlocWithAttn(pre_channel, pre_channel, noise_level_emb_dim=noise_level_channel, norm_groups=norm_groups,
-                               dropout=dropout, with_attn=True),
-            ResnetBlocWithAttn(pre_channel, pre_channel, noise_level_emb_dim=noise_level_channel, norm_groups=norm_groups,
-                               dropout=dropout, with_attn=False)
-        ])
+        # 定义中间层
+        self.mid = nn.ModuleList(
+            [
+                ResnetBlocWithAttn(
+                    pre_channel,
+                    pre_channel,
+                    noise_level_emb_dim=noise_level_channel,
+                    norm_groups=norm_groups,
+                    dropout=dropout,
+                    with_attn=True,
+                ),
+                ResnetBlocWithAttn(
+                    pre_channel,
+                    pre_channel,
+                    noise_level_emb_dim=noise_level_channel,
+                    norm_groups=norm_groups,
+                    dropout=dropout,
+                    with_attn=False,
+                ),
+            ]
+        )
 
+        # 定义上采样层
         ups = []
         for ind in reversed(range(num_mults)):
-            is_last = (ind < 1)
-            use_attn = (now_res in attn_res)
+            is_last = ind < 1
+            use_attn = now_res in attn_res
             channel_mult = inner_channel * channel_mults[ind]
-            for _ in range(0, res_blocks+1):
-                ups.append(ResnetBlocWithAttn(
-                    pre_channel+feat_channels.pop(), channel_mult, noise_level_emb_dim=noise_level_channel, norm_groups=norm_groups,
-                        dropout=dropout, with_attn=use_attn))
+            for _ in range(0, res_blocks + 1):
+                ups.append(
+                    ResnetBlocWithAttn(
+                        pre_channel + feat_channels.pop(),
+                        channel_mult,
+                        noise_level_emb_dim=noise_level_channel,
+                        norm_groups=norm_groups,
+                        dropout=dropout,
+                        with_attn=use_attn,
+                    )
+                )
                 pre_channel = channel_mult
             if not is_last:
                 ups.append(Upsample(pre_channel))
-                now_res = now_res*2
+                now_res = now_res * 2
 
         self.ups = nn.ModuleList(ups)
 
-        self.final_conv = Block(pre_channel, default(out_channel, in_channel), groups=norm_groups)
+        # 定义最终的卷积层
+        self.final_conv = Block(
+            pre_channel, default(out_channel, in_channel), groups=norm_groups
+        )
 
+    # 前向传播函数
     def forward(self, x, time):
-        t = self.noise_level_mlp(time) if exists(
-            self.noise_level_mlp) else None
+        # 如果存在噪声级别的多层感知机，则获取噪声级别
+        t = self.noise_level_mlp(time) if exists(self.noise_level_mlp) else None
 
         feats = []
+        # 通过下采样层
         for layer in self.downs:
             if isinstance(layer, ResnetBlocWithAttn):
                 x = layer(x, t)
@@ -244,16 +315,19 @@ class UNet(nn.Module):
                 x = layer(x)
             feats.append(x)
 
+        # 通过中间层
         for layer in self.mid:
             if isinstance(layer, ResnetBlocWithAttn):
                 x = layer(x, t)
             else:
                 x = layer(x)
 
+        # 通过上采样层
         for layer in self.ups:
             if isinstance(layer, ResnetBlocWithAttn):
                 x = layer(torch.cat((x, feats.pop()), dim=1), t)
             else:
                 x = layer(x)
 
+        # 通过最终的卷积层并返回结果
         return self.final_conv(x)
